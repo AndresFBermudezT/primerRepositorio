204bb52995285f8b1a19a279880037f29e7b27fc
AndresFBermudezT <61174375+AndresFBermudezT@users.noreply.github.com>
2020-02-26 23:39:53 -0500

    Add files via upload

---
+++ primerRepositorio-master/lenguaje.py
@@ -0,0 +1,10 @@
+class Alfabeto():
+    def __init__(self):
+        self.num1 = 1
+        self.num2 = 2
+    def suma(num1,num2):
+print(objeto.num2)
+print("Hola wey")
+
+print("j")
+wait= input("PRESS ENTER TO CONTINUE.")
